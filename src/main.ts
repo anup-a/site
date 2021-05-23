47cc968b1e28eb69e48207d7419923e0093640e3
anup-a <aglawe.anup22@gmail.com>
2021-05-23 16:37:42 +0530

    refactored code + refined loading animation

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,28 @@
+const main = document.getElementById("main-container");
 const loaderWrapper = document.getElementById("wrapper");
+const svglogo = document.getElementById("svglogo");
+const navLogo = document.getElementById("nav-logo");
+const content = document.getElementById("content");
+const illustration = document.getElementById("illustration");
+
+const navRect = navLogo?.getBoundingClientRect();
+const loaderRect = svglogo?.getBoundingClientRect();
+
+const adjustHeight = (navRect?.top || 0) - (loaderRect?.top || 0);
+const adjustWidth = (navRect?.left || 0) - (loaderRect?.left || 0);
+
+const toggleElement = (
+  element: HTMLElement | null,
+  status: "show" | "hide"
+) => {
+  if (status === "show") {
+    element?.classList.add("show");
+    element?.classList.remove("hide");
+  } else {
+    element?.classList.add("hide");
+    element?.classList.remove("show");
+  }
+};
 
 window.onreadystatechange = () => {
   //@ts-ignore
@@ -6,8 +30,17 @@ window.onreadystatechange = () => {
 };
 
 const hideLoader = () => {
-  loaderWrapper?.classList.remove("show");
-  loaderWrapper?.classList.add("hide");
+  console.log(adjustHeight, adjustWidth);
+  if (svglogo) {
+    // svglogo.classList.add("translate-to-origin");
+    svglogo.style.transform = `translateX(${adjustWidth}px) translateY(${adjustHeight}px) scale(0.5)`;
+  }
+  setInterval(() => {
+    toggleElement(loaderWrapper, "hide");
+    toggleElement(content, "show");
+    toggleElement(illustration, "show");
+    main?.classList.remove("over-y-hidden");
+  }, 600);
 };
 
 window.onload = () => {
